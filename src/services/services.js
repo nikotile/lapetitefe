babcb171662f71ad2dbf7f8ef10552129ecbdcd1
Niko <niko@nikotile.xyz>
2022-06-29 10:24:11 +0900

    Make the url relative since ultimately FE and BE is in the same place

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:42069/api/",
+  baseURL: "/api/",
   withCredentials: false,
   headers: {
     Accept: "application/json",
