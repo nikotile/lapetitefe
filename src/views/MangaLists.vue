9bcb277be7cfe8a95a6c12dddbd4b736d23301bc
Niko <niko@nikotile.xyz>
2022-07-05 14:36:48 +0900

    Dirty pagination done dirt cheap
    Since this is only a small web app

--- src/views/MangaLists.vue
+++ src/views/MangaLists.vue
@@ -18,7 +18,7 @@
           <h2 class="text-white font-poppins font-semibold text-l mb-5">Series</h2>
           <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
             <MangaCard
-              v-for="(items, index) in series.slice(0, 6)"
+              v-for="(items, index) in series.slice(0, max)"
               :key="index"
               :manga="items"/>
           </div>
@@ -27,16 +27,32 @@
           <h2 class="text-white font-poppins font-semibold text-l mb-5">Doujin</h2>
           <div class="grid grid-cols-2 lg:grid-cols-2 gap-4">
             <MangaCard
-              v-for="(items, index) in doujin.slice(0, 6)"
+              v-for="(items, index) in doujin.slice((max * page) - max, max * page)"
               :key="index"
               :manga="items"/>
           </div>
         </div>
-        <div class="flex flex-row justify-center">
-          <router-link :to="{ name: 'manga-catalog' }">
-            <button class="button py-1 px-2 text-white rounded shadow-md rounded-md transform transition duration-300 ease-in-out hover:translate-x-1 hover:shadow-lg bg-secondary hover:text-yellow">Katalog ➙</button>
-          </router-link>
+
+        <div class="flex flex-col justify-center items-center mt-6">
+          <div class="flex flex-row mx-auto px-2 font-poppins font-semibold text-white">
+            <button
+              @click="previousPage"
+              :disabled="page <= 1"
+              class="rounded-l-md px-3 transition duration-300 ease-in-out focus:outline-none"
+              :class="[page <= 1 ? 'bg-gray-700' : 'bg-teriary hover:text-yellow hover:-translate-x-1']">
+              <Icon name="arrow-left" />
+            </button>
+            <div class="text-center bg-main w-10 py-2 px-3 "> {{ page }} </div>
+            <button
+              @click="nextPage"
+              :disabled="page === length"
+              :class="[page === length ? 'bg-gray-700' : 'bg-teriary hover:text-yellow hover:translate-x-1']"
+              class="bg-teriary rounded-r-md px-3 transition duration-300 ease-in-out focus:outline-none">
+              <Icon name="arrow-right" />
+            </button>
+          </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -55,21 +71,20 @@
     computed: {
       series() { return this.mangas.filter(manga => manga.data.series) },
       doujin() { return this.mangas.filter(manga => !manga.data.series) },
+      length() {
+        // 'max' content per page
+        const div = this.mangas.filter(manga => !manga.data.series).length / this.max;
+        return Math.ceil(div);
+      },
     },
     data() {
       return {
         mangas: [],
         page: 1,
+        max: 6,
         loading: true,
         error: false,
       };
-    },
-    watch: {
-      page: {
-        handler: 'fetchData',
-        deep: true,
-        immediate: true
-      },
     },
     methods: {
       nextPage() {
@@ -79,7 +94,6 @@
         this.page -= 1;
       },
       async fetchData() {
-        let page = this.page;
         this.loading = true;
 
         Service.getMangas()
@@ -87,7 +101,6 @@
             this.mangas = response.data;
           })
           .catch((error) => {
-            console.log("sorry there was an error " + error);
             this.error = true;
           })
           .finally(() => (this.loading = false));
